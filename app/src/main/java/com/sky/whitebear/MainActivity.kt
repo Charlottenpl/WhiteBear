d74958462c5fc92cb1602061da493b45786f7043
Charlottepl <chenzhongyou@topjoy.com>
2023-03-01 00:41:08 +0800

    add player

--- app/src/main/java/com/sky/whitebear/MainActivity.kt
+++ app/src/main/java/com/sky/whitebear/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.sky.whitebear
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sky.whitebear.ui.theme.WhiteBearTheme
 
@@ -19,6 +25,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             skyView()
         }
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+            intent.putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+            startActivity(intent)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "channel_id"
+            val channelName = "channel_name"
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        } else {
+            // 如果 Android 版本小于 O，则无需请求权限
+        }
+
+
+        // 启动 MusicService
+        var musicServiceIntent = Intent(this, MusicService::class.java)
+        startService(musicServiceIntent)
     }
 }
 
---
+++ app/src/main/java/com/sky/whitebear/MusicService.kt
@@ -0,0 +1,174 @@
+package com.sky.whitebear
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.media.MediaBrowserServiceCompat
+import androidx.media.app.NotificationCompat.MediaStyle
+import androidx.media.session.MediaButtonReceiver
+
+/**
+ *
+ * @author: charlotte
+ * @date: 2023/2/28
+ */
+class MusicService : MediaBrowserServiceCompat() {
+
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var notificationManager: NotificationManagerCompat
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.e("MusicService","MusicService 启动")
+
+
+
+        // 初始化媒体会话和媒体播放器
+        mediaSession = MediaSessionCompat(this, "MusicService")
+        mediaPlayer = MediaPlayer()
+        mediaPlayer.setDataSource("http://music.163.com/song/media/outer/url?id=25906124.mp3")
+
+        // 设置媒体会话的回调
+        mediaSession.setCallback(MediaSessionCallback())
+
+        // 初始化通知管理器
+        notificationManager = NotificationManagerCompat.from(this)
+
+        // 启动媒体会话
+        sessionToken = mediaSession.sessionToken
+
+        mediaSession.isActive = true
+
+        mediaPlayer.setOnPreparedListener {
+            // 当音频准备好时，开始播放音乐
+            mediaPlayer.start()
+        }
+
+        mediaPlayer.prepareAsync()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // 释放媒体播放器
+        mediaPlayer.release()
+
+        // 停止媒体会话
+        mediaSession.release()
+    }
+
+    override fun onGetRoot(
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
+    ): BrowserRoot? {
+        // 返回媒体浏览器的根节点
+        return BrowserRoot("root", null)
+    }
+
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
+        // 返回媒体浏览器的子节点
+        result.sendResult(mutableListOf())
+    }
+
+    inner class MediaSessionCallback : MediaSessionCompat.Callback() {
+
+        override fun onPlay() {
+            mediaPlayer.start()
+            showNotification()
+        }
+
+        override fun onPause() {
+            mediaPlayer.pause()
+            showNotification()
+        }
+
+        override fun onStop() {
+            mediaPlayer.stop()
+            showNotification()
+            stopSelf()
+        }
+
+    }
+
+    private fun showNotification() {
+        // 创建一个通知渠道
+        val channelId = "my_channel"
+        val channelName = "My Channel"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val notificationChannel = NotificationChannel(channelId, channelName, importance)
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setStyle(
+                MediaStyle()
+                    .setMediaSession(mediaSession.sessionToken)
+                    .setShowActionsInCompactView(0, 1, 2)
+                    .setShowCancelButton(true)
+                    .setCancelButtonIntent(
+                        MediaButtonReceiver.buildMediaButtonPendingIntent(
+                            this,
+                            PlaybackStateCompat.ACTION_STOP
+                        )
+                    )
+            )
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("音乐播放器")
+            .setContentText("正在播放")
+
+        builder.addAction(
+            NotificationCompat.Action(
+                R.drawable.previous,
+                "上一首",
+                MediaButtonReceiver.buildMediaButtonPendingIntent(
+                    this,
+                    PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
+                )
+            )
+        )
+
+        builder.addAction(
+            NotificationCompat.Action(
+                R.drawable.pause,
+                "暂停",
+                MediaButtonReceiver.buildMediaButtonPendingIntent(
+                    this,
+                    PlaybackStateCompat.ACTION_PAUSE
+                )
+            )
+        )
+
+        builder.addAction(
+            NotificationCompat.Action(
+                R.drawable.next,
+                "下一首",
+                MediaButtonReceiver.buildMediaButtonPendingIntent(
+                    this,
+                    PlaybackStateCompat.ACTION_SKIP_TO_NEXT
+                )
+            )
+        )
+
+        var notification = builder.build()
+        startForeground(1, notification)
+
+    }
+}
